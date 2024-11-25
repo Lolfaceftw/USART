9db699609c483b0bf77073eaa571c117b9bdedf5
Christian Klein C. Ramos <ckcr20@gmail.com>
2024-11-25 10:35:46 +0800

    Init

---
+++ eee158_mod5/EEE158_Mod05_Exercise_Template.X/main.c
@@ -0,0 +1,9 @@
+/*
+ * This file contains no code, just comments.
+ * 
+ * You are expected to replace the contents of this file with your solution,
+ * starting with the sample code. This exercise is NOT meant to be done fully
+ * from scratch.
+ * 
+ * - EEE 158 Handlers
+ */
---
+++ eee158_mod5/EEE158_Mod05_Exercise_Template.X/platform/usart.c
@@ -0,0 +1,9 @@
+/*
+ * This file contains no code, just comments.
+ * 
+ * You are expected to replace the contents of this file with a completed
+ * version of platform/usart.c from the sample code, with edits to comply with
+ * the specified serial-port configuration.
+ * 
+ * - EEE 158 Handlers
+ */
